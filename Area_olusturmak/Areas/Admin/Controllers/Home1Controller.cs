5edd7a87f844f5dcc576f2e6fd8d25b1fe9bae64
alicanyilmazz <alicanyilmazz@users.noreply.github.com>
2019-04-29 01:11:07 +0300

    Add project files.

---
+++ Area_olusturmak/Areas/Admin/Controllers/Home1Controller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Area_olusturmak.Areas.Admin.Controllers
+{
+    public class Home1Controller : Controller
+    {
+        // GET: Admin/Home1
+        public ActionResult Index()
+        {
+            return RedirectToAction("Index","Home2",new { area="Customer",alican=5}); //With routeValues
+            //return RedirectToAction("Index", "Home2", new { area = "Customer"});    //Without routeValues
+        }
+    }
+}
---
+++ Area_olusturmak/Areas/SuperUser/Controllers/Home3Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Area_olusturmak.Areas.SuperUser.Controllers
+{
+    public class Home3Controller : Controller
+    {
+        // GET: SuperUser/Home3
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
